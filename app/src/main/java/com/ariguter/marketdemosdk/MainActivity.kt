388db2f6e7f6b7f2ac850b04d6352d700de3713f
Gutismall <ari.guter@gmail.com>
2025-05-26 17:05:02 +0300

    init

--- app/src/main/java/com/ariguter/marketdemosdk/MainActivity.kt
+++ app/src/main/java/com/ariguter/marketdemosdk/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ariguter.marketsdk.CategoryDTO
 
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +18,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val cat:
     }
 }
