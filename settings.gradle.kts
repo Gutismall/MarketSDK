6160020a63d3865bcafb62e0e937a436d560609d
Gutismall <ari.guter@gmail.com>
2025-05-26 16:38:39 +0300

    init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,11 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io")}
-        }
+        maven("https://jitpack.io")
+    }
 }
 
 rootProject.name = "MarketDemoSDK"
